a419f587ec46109b9c9e2c496a15bed55f8c847a
Sparky Bluefang <sparky@bluefang-logic.com>
2024-12-09 00:24:08 -0500

    Add options to call generate-tz-header as part of the build process.

--- plugins/color/generate-tz-header.py
+++ plugins/color/generate-tz-header.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import re
+import argparse, os, re
+
+COORDS_RE = re.compile(r"([+-])([0-9]+)([+-])([0-9]+)")
 
 d = {}
 
+parser = argparse.ArgumentParser(
+             prog='generate-tz-header',
+             description='Generate tz-coords.h header from timezone-data')
+parser.add_argument('zoneinfo_path', nargs='?', default='/usr/share/zoneinfo')
+parser.add_argument('-d', '--deprecated', action='store_true')
 
-with open("/usr/share/zoneinfo/zone.tab", "r") as f:
+args = parser.parse_args()
+zoneTab = os.path.join(args.zoneinfo_path, 'zone.tab' if args.deprecated else 'zone1970.tab')
+
+with open(zoneTab, "r") as f:
     for line in f:
-        if line.startswith("#"):
+        line = line.strip()
+        if not line or line.startswith("#"):
             continue
 
-        res = re.search(r"([A-Z]{2})\s([0-9-+]+)\s([\w/_\-]+)\s", line)
-        code, coords, tz = res.groups()
-
-        res = re.search(r"([+-]{1})([0-9]+)([+-]{1})([0-9]+)", coords)
-        lat_sign, lat_val, long_sign, long_val = res.groups()
+        coords, tz = line.split('\t')[1:3]
+        lat_sign, lat_val, long_sign, long_val = COORDS_RE.search(coords).groups()
 
         lat_str = lat_sign + lat_val[0:2] + "." + lat_val[2:]
         long_str = long_sign + long_val[0:3] + "." + long_val[3:]
@@ -25,17 +33,17 @@ with open("/usr/share/zoneinfo/zone.tab", "r") as f:
         d[tz] = [lat, long]
 
 header = """
-// Generated from /usr/share/zoneinfo/zone.tab, used by csd-nightlight.c to calculate sunrise and sunset based on the system timezone
+// Generated from %s, used by csd-nightlight.c to calculate sunrise and sunset based on the system timezone
 
 typedef struct
 {
-    const char *timezone;
+    const gchar *timezone;
     double latitude;
     double longitude;
 } TZCoords;
 
 static TZCoords tz_coord_list[] = {
-"""
+""" % (zoneTab)
 
 for zone in sorted(d.keys()):
     latitude, longitude = d[zone]
